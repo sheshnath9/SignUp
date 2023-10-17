3b275ad6c464a23f9e0de54ca4d0ff21d451cfb4
sheshnath9 <147276087+sheshnath9@users.noreply.github.com>
2023-10-17 23:53:10 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,13 @@
+<?php
+if ($_POST["id"]=="rajdeep"){
+    if($_POST["password"]=="raj123"){
+        echo"Wellcome you logged in successfully";
+    }
+    else{
+        echo "No Entery Your cridential Are worng ! please login Again";
+    }
+}
+else{
+    echo "No Entery Your cridential Are worng ! please login Again";
+}
+?>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+    <form action="about.php" method="post">
+        <input type="text" name="id" placeholder="Enter User Id">
+        <br>
+        <input type="password" name="password" placeholder="password">
+        <br>
+        <input type="submit">
+
+    </form>
+</body>
+</html>
